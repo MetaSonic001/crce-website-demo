c5bb941dad36fb9f1708d9195ca0feb392f74223
ZaneFerns360 <zane.ferns360@gmail.com>
2024-06-27 00:58:23 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Footer from '@/components/Footer';
-import Navbar from '../components/Navbar';
-import Homepage from '@/components/Homepage';
+import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import Homepage from "@/components/Homepage";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Navbar />
       <Homepage />
-      <Footer/>
     </div>
   );
 }
+
