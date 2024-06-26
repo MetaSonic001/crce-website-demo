0e68c09483e23c753e5303803348a3e113ccfaf7
ZaneFerns360 <zane.ferns360@gmail.com>
2024-06-27 01:22:44 +0530

    changes

--- src/components/homepage/QuickGlance.tsx
+++ src/components/homepage/QuickGlance.tsx
@@ -1,54 +1,80 @@
-// src/components/QuickGlance.jsx
-import React from 'react';
+import React from "react";
 
 const QuickGlance = () => {
   return (
-    <aside className="w-1/4 bg-yellow-300/60 rounded-lg p-4">
-      <h2 className="text-lg font-bold mb-4">Quick Glance</h2>
+    <aside className="w-full lg:w-1/3 rounded-lg p-4">
+      <div className="flex flex-row items-center mb-4">
+        <div className="h-[1rem] bg-blue-800 w-1"></div>
+        <span className="font-bold px-1 py-1">Quick Glance</span>
+      </div>
       <ul className="space-y-2">
         <li>
-          <a href="#" className="block bg-purple-600 text-white p-2 rounded">
+          <a
+            href="#"
+            className="block bg-[#5b06f5] text-white py-2 px-4 rounded text-center font-semibold hover:bg-[#f9da4c] hover:text-black"
+          >
             FE Admissions (2024-25)
           </a>
         </li>
         <li>
-          <a href="#" className="block bg-purple-400 text-white p-2 rounded">
+          <a
+            href="#"
+            className="block bg-[#a406f5] text-white py-2 px-4 rounded text-center font-semibold hover:bg-[#f9da4c] hover:text-black"
+          >
             FE Admission Enquiry
           </a>
         </li>
         <li>
-          <a href="#" className="block bg-purple-600 text-white p-2 rounded">
+          <a
+            href="#"
+            className="block bg-[#a406f5] text-white py-2 px-4 rounded text-center font-semibold hover:bg-[#f9da4c] hover:text-black"
+          >
             CRCE Prospectus 2024
           </a>
         </li>
         <li>
-          <a href="#" className="block bg-green-600 text-white p-2 rounded">
+          <a
+            href="#"
+            className="block bg-green-800 text-white py-2 px-4 rounded text-center font-semibold hover:bg-[#f9da4c] hover:text-black"
+          >
             Campus Tour
           </a>
         </li>
         <li>
-          <a href="#" className="block bg-red-600 text-white p-2 rounded">
+          <a
+            href="#"
+            className="block bg-[#f50609] text-white py-2 px-4 rounded text-center font-semibold hover:bg-[#f9da4c] hover:text-black"
+          >
             NIRF
           </a>
         </li>
         <li>
-          <a href="#" className="block bg-blue-600 text-white p-2 rounded">
+          <a
+            href="#"
+            className="block bg-[#3f51b4] text-white py-2 px-4 rounded text-center font-semibold hover:bg-[#f9da4c] hover:text-black"
+          >
             NISP
           </a>
         </li>
         <li>
-          <a href="#" className="block bg-blue-600 text-white p-2 rounded">
+          <a
+            href="#"
+            className="block bg-[#3f51b4] text-white py-2 px-4 rounded text-center font-semibold hover:bg-[#f9da4c] hover:text-black"
+          >
             Alumni Spotlight
           </a>
         </li>
         <li>
-          <a href="#" className="block bg-blue-600 text-white p-2 rounded">
+          <a
+            href="#"
+            className="block bg-[#3f51b4] text-white py-2 px-4 rounded text-center font-semibold hover:bg-[#f9da4c] hover:text-black"
+          >
             Students Bytes
           </a>
         </li>
       </ul>
     </aside>
   );
-}
+};
 
 export default QuickGlance;
